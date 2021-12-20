16c09360fe58092ffd81db1a446b5c49e4b5e9cd
Peter Gordebeke <pgordebeke@gmail.com>
2021-12-20 21:39:11 +0100

    tests

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,13 +22,13 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl: 'https://github.com/09peter/Maragh',
+          editUrl: 'https://github.com/09peter/Maragh/tree/main',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/09peter/Maragh',
+            'https://github.com/09peter/Maragh/tree/main',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
